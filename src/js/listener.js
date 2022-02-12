6e8cb9ec6817136ef8b73340a42817546c25592e
sla6ak <slabakxaker@gmail.com>
2022-02-12 05:24:40 +0200

    карта в футере

--- src/js/listener.js
+++ src/js/listener.js
@@ -11,5 +11,21 @@ export class Listener extends Thema {
   listenerAll = () => {
     this.refs.themaBt.addEventListener('click', this.onThemaClick);
     this.refs.languageTxt.addEventListener('click', this.onLanguageClick);
+    this.refs.mapCity.addEventListener('click', this.onModalMapClick);
+  };
+  // =====================footer map==========================
+  onModalMapClick = () => {
+    const back = this.refs.backdropMap;
+    this.refs.body.classList.add('body__no-scroll');
+    back.classList.remove('displey-hidden');
+    const onBackClick = evn => {
+      if (evn.target !== back) {
+        return;
+      }
+      back.classList.add('displey-hidden');
+      this.refs.body.classList.remove('body__no-scroll');
+      back.removeEventListener('click', onBackClick);
+    };
+    back.addEventListener('click', onBackClick);
   };
 }
